1e9762d1d40517c2256ec498a13686dd465da0f8
Dmytro <dmytrywork81@gmail.com>
2023-06-05 12:54:55 +0300

    05/06/2023

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,50 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/alerts');
   });
 
-  it('should have the ability to assert automatically resolved alerts', () => {
-
+  it('should assert the text inside the alert when the first button is clicked', () => {
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('You clicked a button');
+    });
   });
 
-  it('should have the ability to assert scheduled allert', () => {
-
+  it('should assert the text inside the alert after 5 seconds when the second button is clicked', () => {
+    cy.get('#timerAlertButton').click();
+    cy.wait(5000);
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
-  it('should autimatically resolve alerts', () => {
+  it('should automatically handle resolved alerts when the third button is clicked', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'confirm').returns(true);
+    });
 
+    cy.get('#confirmButton').click();
+
+    cy.get('#confirmResult').should('contain', 'You selected Ok');
   });
 
-  it('should have the ability to Cancel alerts', () => {
+  it('should be able to cancel alerts when the third button is clicked and Cancel is selected', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'confirm').returns(false);
+    });
 
+    cy.get('#confirmButton').click();
+
+    cy.get('#confirmResult').should('contain', 'You selected Cancel');
   });
 
-  it('should have the ability to enter text to alert', () => {
+  it('should be able to enter text into an alert when the fourth button is clicked', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('Dmytro');
+    });
 
+    cy.get('#promtButton').click();
+
+    cy.get('#promptResult').should('contain', 'You entered Dmytro');
   });
 });

ef39d87fe79ba11edbc792bf2067ae5575f41c09
Danylo Kravtsov <kravtsovdania@gmail.com>
2023-06-06 02:29:12 +0300

    Solution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('getById', (id) => {
+    cy.get(`#${id}`);
+});

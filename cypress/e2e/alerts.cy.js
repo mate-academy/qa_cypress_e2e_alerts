7c19b11d67238c847244c965877c19aee53282c2
BakumSV <immigrantsong61@gmail.com>
2023-06-05 19:47:25 +0300

    Add the last task

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -11,8 +11,8 @@ describe('Cypress application', () => {
     });
   });
 
-  it('should have the ability to assert scheduled allert', () => {
-    cy.get('[id="alertButton"]').click();
+  it('should have the ability to assert scheduled alert', () => {
+    cy.get('#timerAlertButton').click();
 
     cy.on('window:alert', (text) => {
       expect(text).to.contains('This alert appeared after 5 seconds');
@@ -43,11 +43,19 @@ describe('Cypress application', () => {
       .should('have.text', 'You selected Cancel');
   });
 
-  // it.only('should have the ability to enter text to alert', () => {
-  //   cy.get('#promtButton').click();
+  it('should have the ability to enter text to alert', () => {
+    cy.window()
+      .then((win) => {
+        cy.stub(win, 'prompt').returns('Serhii Bakumenko');
+      });
 
-  //   cy.on('window:confirm', (text) => {
-  //     expect(text).to.contains('Please enter your name');
-  //   });
-  // });
+    cy.get('#promtButton').click();
+
+    cy.on('window:confirm', (text) => {
+      expect(text).to.contains('Please enter your name');
+    });
+
+    cy.get('#promptResult')
+      .should('have.text', 'You entered Serhii Bakumenko');
+  });
 });

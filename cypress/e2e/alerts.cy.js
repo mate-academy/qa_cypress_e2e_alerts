99bcb356c86d4bbaa3df8bc6a1144cc3a0aeaeda
Oleksii Shypovalov <asipovalov9@gmail.com>
2023-06-06 14:10:14 +0300

    Fixed1

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -45,7 +45,7 @@ describe('Cypress application', () => {
     .should('contain', 'You selected Cancel');
   });
 
-  it.only('should have the ability to enter text to alert', () => {
+  it('should have the ability to enter text to alert', () => {
       cy.window().then((win) => {
       cy.stub(win, 'prompt').returns('Test')
       cy.get('#promtButton')

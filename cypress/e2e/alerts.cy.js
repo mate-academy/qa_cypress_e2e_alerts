7c0966dcb6c63073703ab2074d0a405bf5675b12
Antonii Zaluzhnyi <anton.zaluzhnuyi@gmail.com>
2023-07-21 17:09:58 +0300

    solution

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,41 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('You clicked a button');
+    });
   });
 
   it('should have the ability to assert scheduled allert', () => {
+    cy.get('#timerAlertButton').click().wait(5000);
 
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
   it('should autimatically resolve alerts', () => {
+    cy.get('#confirmButton').click();
 
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('Do you confirm action?');
+      return true;
+    });
+    cy.get('#confirmResult').should('contain', 'You selected Ok');
   });
 
-  it('should have the ability to Cancel alerts', () => {
+  it.only('should have the ability to Cancel alerts', () => {
+    cy.get('#confirmButton').click();
 
-  });
-
-  it('should have the ability to enter text to alert', () => {
-
+    cy.on('window:confirm', (text) => {
+      expect(text).to.equal('Do you confirm action?');
+      return false;
+    });
+    cy.get('#confirmResult').should('contain', 'You selected Cancel');
   });
 });
+

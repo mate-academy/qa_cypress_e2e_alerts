a025ca664f1f5aa80992650a938df9171260a896
Andrii Chuzhyk <andrey.chuzhik@gmail.com>
2023-07-21 16:00:23 +0300

    solution alerts

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/alerts',
+    viewportHeight: 1200,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,45 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('/');
   });
 
+  // eslint-disable-next-line max-len
   it('should have the ability to assert automatically resolved alerts', () => {
+    cy.findById('alertButton').click();
 
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`You clicked a button`);
+    });
   });
 
   it('should have the ability to assert scheduled allert', () => {
+    cy.findById('timerAlertButton').click();
 
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000);
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`This alert appeared after 5 seconds`);
+    });
   });
-
   it('should autimatically resolve alerts', () => {
+    cy.findById('confirmButton').click();
 
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal(`Do you confirm action?`);
+    });
+
+    cy.findById('confirmResult').should('contain.text', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
+    cy.findById('confirmButton').click();
 
-  });
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal(`Do you confirm action?`);
+      return false;
+    });
 
-  it('should have the ability to enter text to alert', () => {
-
+    cy.findById('confirmResult').should('contain.text', 'You selected Cancel');
   });
 });

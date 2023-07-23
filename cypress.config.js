4056c40fe781e40f2d034a8d8a170a8e82c01175
Roman Miskiv <molfargoods@gmail.com>
2023-07-23 21:30:56 +0300

    Solution alerts

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/alerts',
+    viewportHeight: 1000,
+    viewportWidth: 1200,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }

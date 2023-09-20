32174689c54d54d2d50bd01f543a4075c6328a4d
WesafEnginer <masterarturgames@gmail.com>
2023-09-20 15:51:42 +0300

    Alert

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -4,16 +4,14 @@ describe('Cypress application', () => {
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-    cy.get('#alertButton')
-      .click();
+    cy.get('#alertButton').click();
     cy.on('window:alert', (alert) => {
       expect(alert).to.equal('You clicked a button');
     });
   });
 
   it('should have the ability to assert scheduled allert', () => {
-    cy.get('#timerAlertButton')
-      .click();
+    cy.get('#timerAlertButton').click();
     cy.wait(5000);
     cy.on('window:alert', (alert) => {
       expect(alert).to.equal('This alert appeared after 5 seconds');
@@ -21,36 +19,29 @@ describe('Cypress application', () => {
   });
 
   it('should autimatically resolve alerts', () => {
-    cy.get('#confirmButton')
-      .click();
+    cy.get('#confirmButton').click();
     cy.on('window:alert', (alert) => {
       expect(alert).to.equal('Do you confirm action?');
       return true;
     });
-    cy.get('#confirmResult')
-      .should('contain', 'You selected Ok');
+    cy.get('#confirmResult').should('contain', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
-    cy.get('#confirmButton')
-      .click();
+    cy.get('#confirmButton').click();
     cy.on('window:confirm', (alert) => {
       expect(alert).to.equal('Do you confirm action?');
       return false;
     });
-    cy.get('#confirmResult')
-      .should('contain', 'You selected Cancel');
+    cy.get('#confirmResult').should('contain', 'You selected Cancel');
   });
 
   it('should have the ability to enter text to alert', () => {
     const name = 'Artur';
     cy.window().then((win) => {
-      cy.stub(win, 'prompt')
-        .returns(name);
-      cy.get('#promtButton')
-        .click();
-      cy.get('#promptResult')
-        .should('contain', 'You entered Artur');
+      cy.stub(win, 'prompt').returns(name);
+      cy.get('#promtButton').click();
+      cy.get('#promptResult').should('contain', 'You entered Artur');
     });
   });
 });

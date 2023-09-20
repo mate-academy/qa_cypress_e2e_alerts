5f2d8cece776838d421c3ca7dfd0f8b236f90c3a
Vladyslav Lysenko <vlad.litvin598@gmail.com>
2023-09-20 13:59:35 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/',
+    viewportHeight: 1920,
+    viewportWidth: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,55 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('You clicked a button');
+    });
   });
 
   it('should have the ability to assert scheduled allert', () => {
+    cy.clock();
 
+    cy.get('#timerAlertButton').click();
+
+    cy.tick(5000).then(() => {
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('This alert appeared after 5 seconds');
+    });
+  });
   });
 
   it('should autimatically resolve alerts', () => {
+    cy.get('#confirmButton').click();
 
+    cy.on('window:confirm', (alert) => {
+      expect(alert).to.equal('Do you confirm action?');
+    })
+    cy.get('#confirmResult').should('contain', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
+    cy.get('#confirmButton').click();
 
+    cy.on('window:confirm', (alert) => {
+      expect(alert).to.equal('Do you confirm action?');
+      return false;
+    });
+    cy.get('#confirmResult').should('contain', 'You selected Cancel');
   });
 
   it('should have the ability to enter text to alert', () => {
+    const name = 'Vlad';
 
+    cy.get('#promtButton').click();
+
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns(name);
+    })
+    cy.get('#promtButton').click();
+    cy.get('#promptResult').should('contain', `You entered ${name}`);
   });
 });

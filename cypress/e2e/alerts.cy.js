7c3ee9b34dfcba38a04f7299c1dce54c5b69b42a
KlymSudakov <sudakov.klym@gmail.com>
2023-11-07 15:45:22 +0200

    implement alert tests

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,54 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-
+    cy.get('#alertButton')
+      .click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('You clicked a button');
+    });
   });
 
   it('should have the ability to assert scheduled allert', () => {
-
+    cy.get('#timerAlertButton')
+      .click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
   it('should autimatically resolve alerts', () => {
-
+    cy.get('#confirmButton')
+      .click();
+    cy.on('window:confirm', (alertText) => {
+      expect(alertText).to.equal('Do you confirm action?');
+    });
+    cy.get('#confirmResult')
+      .should('contain', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
+    cy.get('#confirmButton')
+      .click();
+    cy.on('window:confirm', (alertText) => {
+      expect(alertText).to.equal('Do you confirm action?');
 
+      return false;
+    });
+    cy.get('#confirmResult')
+      .should('contain', 'You selected Cancel');
   });
 
   it('should have the ability to enter text to alert', () => {
-
+    cy.window().then((alert) => {
+      cy.get('#promtButton')
+        .click();
+      cy.stub(alert, 'prompt')
+        .returns('Samuel');
+      cy.get('#promptResult')
+        .should('contain', 'You entered Samuel');
+    });
   });
 });

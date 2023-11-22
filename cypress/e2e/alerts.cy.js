7a21b14d0136bc19c2df96bbc5710740af1c2342
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-22 07:47:12 +0200

    alerts.cy.js

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,10 +1,12 @@
 describe('Cypress application', () => {
+  let stub;
+
   before(() => {
-    cy.visit('https://demoqa.com/alerts')
+    stub = cy.stub();
+    cy.visit('https://demoqa.com/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-    const stub = cy.stub()
     cy.on('window:alert', stub)
     cy.get('#alertButton').click().then(() => {
       expect(stub.getCall(0)).to.be.calledWith('I am an alert box!')
@@ -12,7 +14,6 @@ describe('Cypress application', () => {
   });
 
   it('should have the ability to assert scheduled alert', () => {
-    const stub = cy.stub()
     cy.on('window:alert', stub)
     cy.get('#timerAlertButton').click().then(() => {
       expect(stub.getCall(0)).to.be.calledWith('This alert appeared after 5 seconds')
@@ -20,7 +21,6 @@ describe('Cypress application', () => {
   });
 
   it('should automatically resolve alerts', () => {
-    const stub = cy.stub()
     cy.on('window:confirm', stub)
     cy.get('#confirmButton').click().then(() => {
       expect(stub.getCall(0)).to.be.calledWith('Do you confirm action?')
@@ -28,11 +28,9 @@ describe('Cypress application', () => {
   });
 
   it('should have the ability to Cancel alerts', () => {
-    let count = 0
     cy.on('window:confirm', (str) => {
-      count += 1
-      if (count === 1) {
-        expect(str).to.eq('Do you confirm action?')
+      expect(str).to.eq('Do you confirm action?')
+      if (stub.callCount === 1) {
         return false
       }
     })
@@ -41,9 +39,8 @@ describe('Cypress application', () => {
   });
 
   it('should have the ability to enter text to alert', () => {
-    const stub = cy.stub()
     cy.on('window:prompt', stub.withArgs('Please enter your name').returns('Your Name'))
-    cy.get('#promtButton').click()
+    cy.get('#promptButton').click()
     cy.get('#promptResult').should('contain', 'You entered Your Name')
   });
 });

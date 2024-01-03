8edece0e1d946801daaf35806ec475e8e12be945
Rogers1275 <sashakondyuk@gmail.com>
2024-01-03 12:39:35 +0200

    added tests for alerts

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,46 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-
+    cy.findById('alertButton').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`You clicked a button`);
+    });
   });
 
   it('should have the ability to assert scheduled allert', () => {
-
+    cy.findById('timerAlertButton').click();
+    cy.wait(5000);
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`This alert appeared after 5 seconds`);
+    });
   });
 
-  it('should autimatically resolve alerts', () => {
-
+  it('should automatically resolve alerts', () => {
+    cy.findById('confirmButton').click();
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal(`Do you confirm action?`);
+    });
+    cy.findById('confirmResult').should('contain', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
+    cy.findById('confirmButton').click();
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal(`Do you confirm action?`);
+      return false;
+    });
 
+    cy.findById('confirmResult').should('contain', 'You selected Cancel');
   });
 
   it('should have the ability to enter text to alert', () => {
-
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('Oleksandr');
+    });
+    cy.findById('promtButton').click();
+    cy.findById('promptResult').should('contain', 'You entered Oleksandr');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
+});
+
+Cypress.Commands.add('findById', (id) => {
+  cy.get(`[id="${id}"]`);
+});

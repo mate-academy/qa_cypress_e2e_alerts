34c04488d204306c1d82cb3a4cc6ddbe5359fe40
Maksym Tarasov <tarasovmv90@gmail.com>
2024-01-03 16:02:39 +0200

    alerts_cypress_Tarasov

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 800,
+    viewportWidth: 1280,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }

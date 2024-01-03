39dc3fb85382f24c0c00fae1a80a13ea85da85b7
Lyubov Nekroyenko <nekroyenko@gmail.com>
2024-01-03 17:54:53 +0200

    add tests alerts

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/alerts',
+    viewportWidth: 1320,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }

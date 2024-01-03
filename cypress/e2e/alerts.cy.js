47dc329ee69b2a1189dc9cfedaeb0ca156c1bdcd
Alina Lynnyk <lnk3071129@gmail.com>
2024-01-03 14:25:59 +0200

    03.01.2024

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,48 @@
 describe('Cypress application', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
+    cy.get('#alertButton').click();
+    const stub = cy.stub();
 
+    cy.on('window:alert', stub);
+
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy.get('#alertButton')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('You clicked a button');
+      });
   });
 
   it('should have the ability to assert scheduled allert', () => {
-
+    cy.clock();
+    cy.get('#timerAlertButton').click();
+    cy.tick(5000);
+    cy.on('window:alert', (alertText) => {
+      // eslint-disable-next-line max-len
+      expect(alertText).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
   it('should autimatically resolve alerts', () => {
-
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', (alertText) => {
+      expect(alertText).to.equal('Do you confirm action?');
+    });
+    cy.on('window:confirm', () => true);
+    cy.get('.text-success').should('contain', 'You selected Ok');
   });
 
   it('should have the ability to Cancel alerts', () => {
-
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', () => false);
+    cy.get('.text-success').should('contain', 'You selected Cancel');
   });
 
-  it('should have the ability to enter text to alert', () => {
-
-  });
+  // it('should have the ability to enter text to alert', () => {
+  //   cy.get('#promtButton').click();
+  // });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
+});
+
+// Cypress.Commands.add('login', () => {
+//   cy.request({
+//     method: 'POST',
+//     url: 'https://demoqa.com/Account/v1/Login',
+//   });
+// });

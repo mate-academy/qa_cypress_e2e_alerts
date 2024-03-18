2db0e44e0459303b6ed018ab5a55f1c603cf43f5
Tetiana Maksiuk <tetianamaksiuk@gmail.com>
2024-03-18 15:20:07 +0200

    Tetiana_Maksiuk

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,65 @@
-describe('Cypress application', () => {
-  before(() => {
+/// <reference types='cypress' />
 
+const textSeeAlert = "You clicked a button";
+const textAfter5secondsAlert = "This alert appeared after 5 seconds";
+const textConfirmAlert = "Do you confirm action?";
+const textSelected = "You selected ";
+const textOk = "Ok";
+const textCancel = "Cancel";
+const textEntername = "Please enter your name";
+const textPromptAlert = "Tetiana Maksiuk";
+
+describe("Cypress application", () => {
+  beforeEach(() => {
+    cy.visit("/alerts");
   });
 
-  it('should have the ability to assert automatically resolved alerts', () => {
-
+  it("should have the ability to assert automatically resolved alerts", () => {
+    cy.findById("alertButton").click();
+    cy.on("window:alert", (textOfAlert) => {
+      expect(textOfAlert).to.equal(textSeeAlert);
+    });
   });
 
-  it('should have the ability to assert scheduled allert', () => {
-
+  it("should have the ability to assert scheduled allert", () => {
+    cy.clock();
+    cy.findById("timerAlertButton").click();
+    //cy.wait(5000);
+    cy.tick(5000);
+    cy.on("window:alert", (textOfAlert) => {
+      expect(textOfAlert).to.equal(textAfter5secondsAlert);
+    });
   });
 
-  it('should autimatically resolve alerts', () => {
-
+  it("should autimatically resolve alerts", () => {
+    cy.findById("confirmButton").click();
+    cy.on("window:confirm", (textOfConfirm) => {
+      expect(textOfConfirm).to.equal(textConfirmAlert);
+      return true;
+    });
+    cy.findById("confirmResult").should(
+      "have.text",
+      `${textSelected}${textOk}`
+    );
   });
 
-  it('should have the ability to Cancel alerts', () => {
-
+  it("should have the ability to Cancel alerts", () => {
+    cy.findById("confirmButton").click();
+    cy.on("window:confirm", (textOfConfirm) => {
+      expect(textOfConfirm).to.equal(textConfirmAlert);
+      return false;
+    });
+    cy.findById("confirmResult").should(
+      "have.text",
+      `${textSelected}${textCancel}`
+    );
   });
 
-  it('should have the ability to enter text to alert', () => {
-
+  it("should have the ability to enter text to alert", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "prompt").returns(textPromptAlert);
+    });
+    cy.findById("promtButton").click();
+    cy.findById("promptResult").should("contain", `${textPromptAlert}`);
   });
 });

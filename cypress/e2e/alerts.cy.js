4b4621fa3a4c7478941b31af08dd9f2a5eaafd32
Olena Lissova <lena.lissova@gmail.com>
2024-03-18 15:32:51 +0100

    Solution

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,48 @@
-describe('Cypress application', () => {
-  before(() => {
+/* eslint-disable cypress/no-unnecessary-waiting */
 
+describe('Cypress application', () => {
+  beforeEach(() => {
+    cy.visit('/alerts');
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (string) => {
+      expect(string).to.equal('You clicked a button');
+    });
   });
 
-  it('should have the ability to assert scheduled allert', () => {
-
+  it('should have the ability to assert scheduled alert', () => {
+    cy.get('#timerAlertButton').click();
+    cy.wait(5000);
+    cy.on('window:alert', (string) => {
+      expect(string).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
-  it('should autimatically resolve alerts', () => {
-
+  it('should automatically resolve alerts', () => {
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm ', (string) => {
+      expect(string).to.equal('Do you confirm action?');
+      return true;
+    });
   });
 
   it('should have the ability to Cancel alerts', () => {
-
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', (string) => {
+      expect(string).to.equal('Do you confirm action?');
+      return false;
+    });
   });
 
-  it('should have the ability to enter text to alert', () => {
-
+  it.only('should have the ability to enter text to alert', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('Olena');
+    });
+    cy.get('#promtButton').click();
+    cy.on('window:alert', (string) => {
+      expect(string).to.equal('Olena');
+    });
   });
 });

6b276f960c581b9b20c43e1a13cb860798f17750
Oksana Kachmar <profiscribe@gmail.com>
2024-03-18 18:06:08 +0200

    Added fixes

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -4,47 +4,60 @@ describe('Cypress application', () => {
   });
 
   it('should have the ability to assert automatically resolved alerts', () => {
-     cy.get('#alertButton').contains('Click me').click();
-     cy.on('window:alert', (string) => {
-      expect(string).to.equal('You clicked a button');   
+    const alertMessage = 'You clicked a button';
+
+    cy.get('#alertButton').contains('Click me').click();
+    cy.on('window:alert', (string) => {
+    expect(string).to.equal(alertMessage);   
      });
   });
 
   it('should have the ability to assert scheduled allert', () => {
+    const alertMessage5seconds = 'This alert appeared after 5 seconds';
+
     cy.get('#timerAlertButton').contains('Click me').click();
     cy.wait(5000);
     cy.on('window:alert', (string) => {
-      expect(string).to.equal('This alert appeared after 5 seconds');
+      expect(string).to.equal(alertMessage5seconds);
     });
   });
 
   it('should autimatically resolve alerts', () => {
+    const confirmMessage = 'You selected Ok';
+    const confirmQuestion = 'Do you confirm action?';
+
     cy.get('#confirmButton').contains('Click me').click();
     cy.on('window:confirm', (string) => {
-    expect(string).to.equal(`Do you confirm action?`);
+    expect(string).to.equal(confirmQuestion);
     });
     cy.on('window:confirm', () => true);
-    cy.get('#confirmResult').should('contain', 'You selected Ok');
+    cy.get('#confirmResult').should('contain', confirmMessage);
   });
 
   it('should have the ability to Cancel alerts', () => {
+    const confirmQuestion = 'Do you confirm action?';
+    const cancelQuestion = 'You selected Cancel';
+
     cy.get('#confirmButton').contains('Click me').click();
     cy.on('window:confirm', (string) => {
-    expect(string).to.equal(`Do you confirm action?`);
+    expect(string).to.equal(confirmQuestion);
     });
     cy.on('window:confirm', () => false);
-    cy.get('#confirmResult').should('contain', 'You selected Cancel');
+    cy.get('#confirmResult').should('contain', cancelQuestion);
   });
 
   it('should have the ability to enter text to alert', () => {
+    const textEntername = "Please enter your name";
+    const textPromptAlert = "Oksana";
+    
     cy.window().then((win) => {
-    cy.stub(win, 'prompt').returns('Oksana');
+    cy.stub(win, 'prompt').returns(textPromptAlert);
     });
     cy.get('#promtButton').click();
     cy.on('window:alert', (string) => {
-      expect(string).to.equal('Please enter your name');
+      expect(string).to.equal(textEntername);
     });
-    cy.get('#promptResult').should('contain', 'Oksana');
+    cy.get('#promptResult').should('contain', textPromptAlert);
   
   });
 });

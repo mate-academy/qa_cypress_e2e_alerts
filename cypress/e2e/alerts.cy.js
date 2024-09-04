b89560d55090ab5915f3caa09bacd030f08c971f
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-09-04 14:07:52 +0300

    Alerts

--- cypress/e2e/alerts.cy.js
+++ cypress/e2e/alerts.cy.js
@@ -1,25 +1,50 @@
-describe('Cypress application', () => {
-  before(() => {
-
+// <reference types="cypress"/>
+describe('Cypress Alerts', () => {
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/alerts');
   });
 
-  it('should have the ability to assert automatically resolved alerts', () => {
-
+  it('should assert resolved alerts', () => {
+    cy.get('#alertButton').click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('You clicked a button');
+    });
   });
 
-  it('should have the ability to assert scheduled allert', () => {
-
+  it('should assert scheduled alerts', () => {
+    cy.get('#timerAlertButton').click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('This alert appeared after 5 seconds');
+    });
   });
 
-  it('should autimatically resolve alerts', () => {
-
+  it('should resolve confirm alerts', () => {
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', (confirmText) => {
+      expect(confirmText).to.equal('Do you confirm action?');
+      return true;
+    });
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('You selected Ok');
+    });
   });
 
-  it('should have the ability to Cancel alerts', () => {
-
+  it('should cancel confirm alerts', () => {
+    cy.get('#confirmButton').click();
+    cy.on('window:confirm', (confirmText) => {
+      expect(confirmText).to.equal('Do you confirm action?');
+      return false;
+    });
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('You selected Cancel');
+    });
   });
 
-  it('should have the ability to enter text to alert', () => {
-
+  it('should handle prompt alerts', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('Alya');
+    });
+    cy.get('#promtButton').click();
+    cy.get('#promptResult').should('have.text', 'You entered Alya');
   });
 });

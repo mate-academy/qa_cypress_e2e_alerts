78e77d6a09993444731353e7b7ce3b704a30491a
Piotr <piotrwandas66@gmail.com>
2024-09-11 13:19:33 +0200

    Fix the task

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/alerts',
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
-  }
+      on("task", {
+        generateUser() {
+          return {
+            typicatText: faker.lorem.text()
+          };
+        },
+      });
+    },
+  },
 });
